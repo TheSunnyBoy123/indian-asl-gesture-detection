b72b5d46c471f1d5dc12df07b1939f3ce69c4ffb
Sunraj <sunrajpatelcph@gmail.com>
2024-07-15 05:14:18 +0530

    used mediapipe for basic skeleton of hand

--- box-hand.py
+++ box-hand.py
@@ -1,35 +1,37 @@
 import cv2
-import numpy as np
+import mediapipe as mp
 import time
+from tabulate import tabulate
+import os
 
-def image(): 
-    cap = cv2.VideoCapture(0)
-    if not cap.isOpened():
-        print("Error: Could not open camera.")
-        cap.release()
-        return None
-    ret, frame = cap.read()
-    if not ret:
-        cap.release()
-        print("Error: Failed to capture image.")
-        return None
-    else:
-        cap.release()
-        return frame
-
-cv2.namedWindow("Camera", cv2.WINDOW_NORMAL)
+cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands 
+hands = mpHands.Hands() 
+mpDraw = mp.solutions.drawing_utils 
 
 while True:
-    # Capture image
-    img = image()
-    # If image is captured
-    if img is not None:
-        # detect hand using the module cv2.CascadeClassifier
-        hand_cascade = cv2.CascadeClassifier('hand.xml')
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        hands = hand_cascade.detectMultiScale(gray, 1.1, 5)
-        # Draw a box around the hand
-        for (x, y, w, h) in hands:
-            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        # Display the image
-        cv2.imshow("Camera", img)
+    success, img = cap.read() 
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    # Turn img to grayscale
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    if results.multi_hand_landmarks:
+        table_data = []
+        for handlms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handlms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                table_data.append([id, cx, cy])
+                cv2.circle(img, (cx, cy), 15, (0, 0, 255), cv2.FILLED)
+            mpDraw.draw_landmarks(img, handlms, mpHands.HAND_CONNECTIONS)
+            
+        os.system('clear') 
+        print(tabulate(table_data, headers=["ID", "X", "Y"], tablefmt="grid"))
+
+    cv2.imshow("Basic Hand Skeleton", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
