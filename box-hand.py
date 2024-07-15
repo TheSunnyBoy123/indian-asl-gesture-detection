f8bcddb21600140c21f0b1cf521bab1045f54cd1
Sunraj <sunrajpatelcph@gmail.com>
2024-07-15 17:01:00 +0530

    tabulating points

--- box-hand.py
+++ box-hand.py
@@ -13,8 +13,6 @@ while True:
     success, img = cap.read() 
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-    # Turn img to grayscale
-    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     if results.multi_hand_landmarks:
         table_data = []
@@ -25,7 +23,7 @@ while True:
                 table_data.append([id, cx, cy])
                 cv2.circle(img, (cx, cy), 15, (0, 0, 255), cv2.FILLED)
             mpDraw.draw_landmarks(img, handlms, mpHands.HAND_CONNECTIONS)
-            
+
         os.system('clear') 
         print(tabulate(table_data, headers=["ID", "X", "Y"], tablefmt="grid"))
 
@@ -34,4 +32,4 @@ while True:
         break
 
 cap.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() 
